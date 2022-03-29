bd430284df49bf722d373f46e1ebc4c9c4982d85
XuanChen6 <hls.chenxuan666@gmail.com>
2022-03-29 13:14:40 +0200

    update learning 1

--- Ch01 Introduction to R.R
+++ Ch01 Introduction to R.R
@@ -6,6 +6,9 @@
 # A sample R session
 # q() is commented out so you don't accidently exit
 
+#learn R
+x <- rnorm(5)
+x
 # Listing 1.1 - A Sample R session
 
 age <- c(1,3,5,2,11,9,3,9,12,3)
@@ -14,12 +17,23 @@ mean(weight)
 sd(weight)
 cor(age,weight)
 plot(age,weight)
+demo()
+demo(image)
+demo(Hershey)
+demo(persp)
 # q()
 
 
+help.start()
+help("foo") 
+?foo
+??foo
+getwd() 
+ls()
 # Listing 1.2 - An example of commands used to manage the R Workspace. 
 
 setwd("C:/myprojects/project1") # change the path to one of your directories
+setwd("U:/My R Files")
 options()
 options(digits=3)
 x <- runif(20)
@@ -29,6 +43,7 @@ savehistory()
 save.image()
 # q()
 
+q()
 
 # Listing 1.3 - Working with a new package
 
@@ -40,3 +55,9 @@ help(Arthritis)
 Arthritis
 example(Arthritis)
 # q()
+
+#use UU U-drive to save the data
+setwd("U:/My R Files")
+
+
+
